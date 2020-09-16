8543ea17d6e0af36951e226fb2716488bfbeb035
Crowdbotics <team@crowdbotics.com>
2020-09-16 09:07:01 +0000

    Update models

--- status_rdv_20325/wsgi.py
+++ status_rdv_20325/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'status_rdv_20325.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "status_rdv_20325.settings")
 
 application = get_wsgi_application()
